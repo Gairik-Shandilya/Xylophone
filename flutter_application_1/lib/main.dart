7b89f00a1d80f7d98f2b5badccf61bcfeb5045fb
Gairik-Shandilya <gs9277@srmist.edu.in>
2023-03-25 13:08:16 +0530

    Added more sounds

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,8 +1,5 @@
-
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
-import 'package:http/http.dart' as http;
-import 'package:flutter/services.dart';
 
 void main() => runApp(const Xylophonenew());
 
@@ -15,21 +12,42 @@ class Xylophonenew extends StatefulWidget {
 
 class _XylophonenewState extends State<Xylophonenew> {
   AudioPlayer audioPlayer = AudioPlayer();
+  Expanded buildkey({Color? color, int? soundnumber}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: color, // Text Color
+        ),
+        onPressed: () async {
+          await audioPlayer.play(AssetSource("audio/note$soundnumber.wav"));
+        },
+        child: const Text(''),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text("XYLOPHONE")),
+          backgroundColor: Colors.black,
+        ),
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () async{
-                
-                AudioPlayer audioplayer = AudioPlayer();
-                await audioPlayer.play(AssetSource("assets/audio/note1.wav"));
-              }, 
-              child: const Text('Click me', style: TextStyle(fontSize: 30)),
-            ),
-          ),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildkey(color: Colors.red, soundnumber: 1),
+                buildkey(color: Colors.orange, soundnumber: 2),
+                buildkey(color: Colors.yellow, soundnumber: 3),
+                buildkey(color: Colors.green, soundnumber: 4),
+                buildkey(color: Colors.teal, soundnumber: 5),
+                buildkey(color: Colors.blue, soundnumber: 6),
+                buildkey(color: Colors.purple, soundnumber: 7),
+              ]),
         ),
       ),
     );
